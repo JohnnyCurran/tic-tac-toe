c5d70d7525e4b182237f0c4c9b8961eae40fb911
Matthew Spence <mspence5555@gmail.com>
2021-11-08 17:53:59 -0600

    continued work of refactoring code and working on 2d array to hold game board and determine winner

--- script.js
+++ script.js
@@ -13,30 +13,40 @@ let player2 = players("player 2", "o");
 
 
 function initGame () {
-let counter = 0;
+    let counter = 0;
     let gameBoardDivs = document.querySelectorAll(".gameBoardDivs");
+    let displayWhosTurn = document.querySelector(".displayWhosTurn");
+    let whosTurn = player1.name;
+    displayWhosTurn.textContent = whosTurn
+
     gameBoardDivs.forEach((div) => {
         counter++;
         div.classList.add(counter);
+        this["div" + counter] = document.getElementsByClassName([div.classList[1]]);
         
-        
-            this["div" + counter] = document.getElementsByClassName([div.classList[1]])
-            
-            //console.log(div.classList[1])
-            
-    })
-  
-}
+        div.addEventListener("click", () => {
 
-// function gameFlow() {
-//     var whosTurn = player1.name;
-//     let displayWhosTurn = document.querySelector(".displayWhosTurn");
-//     displayWhosTurn.textContent = player1.name;
-    
+            if(whosTurn == player1.name) {
+                div.textContent = player1.symbol;
+                let pointerNum = div.classList[1];
+                for(let y = 0; y < 3; y++) {
+                
+                    for(let x = 0; x < 3; x++) {
+                        if(pointerNum == gameBoard.gameArray[y][x]) {
+                            gameBoard.gameArray[y].splice(x, 1, "x")
+                        };
+                    };
+                };
+                whosTurn == player2.name
+            };
+            
+        });
+    });
+  
+};
+
+
 //     index = gameBoard.gameArray.findIndex(items => items.uniqueId == parseInt(deleteBtn.className))
 //         console.log(index)
 //         //myLibrary.splice(index, 1)
         
-    
-
-// }
