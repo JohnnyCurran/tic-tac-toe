ece1c599eab58ea43e18edd8b10f83fde5f6227f
Matthew Spence <mspence5555@gmail.com>
2021-11-08 16:46:10 -0600

    refactoring the bad code

--- script.js
+++ script.js
@@ -1,73 +1,42 @@
-let gameBoardArr = []
+const players = (name, symbol) => {
+    return { name, symbol };
+};
+
+let gameBoard = {
+    gameArray : [["1", "2", "3"], 
+                ["4", "5", "6"],
+                ["7", "8", "9"]],
+}
+
+let player1 = players("player 1", "x");
+let player2 = players("player 2", "o");
 
 
-function gameFlow() {
-    let whosTurn = "player1";
-    let gameBoard = document.querySelectorAll(".gameBoard");
-    let displayWhosTurn = document.querySelector(".displayWhosTurn");
-    displayWhosTurn.textContent = whosTurn;
-    let boardPosition = 0
-
-    gameBoard.forEach(square => {
-        boardPosition++
-        square.classList.add("position" + boardPosition)
+function initGame () {
+let counter = 0;
+    let gameBoardDivs = document.querySelectorAll(".gameBoardDivs");
+    gameBoardDivs.forEach((div) => {
+        counter++;
+        div.classList.add(counter);
         
-        square.addEventListener("click", () => {
-            if(whosTurn == "player1") {
-                if(square.textContent == "x" || square.textContent == "o") {return}
-                square.textContent = "x";
-                gameBoardArr.push(square.textContent);
-                whosTurn = "player2";
-                displayWhosTurn.textContent = whosTurn;
-                checkWinner();
-            }
-            else if(whosTurn == "player2") {
-                if(square.textContent == "x" || square.textContent == "o") {return}
-                square.textContent = "o";
-                gameBoardArr.push(square.textContent);
-                whosTurn = "player1";
-                displayWhosTurn.textContent = whosTurn;
-                checkWinner();
-            }
-        })
+        
+            this["div" + counter] = document.getElementsByClassName([div.classList[1]])
+            
+            //console.log(div.classList[1])
+            
     })
-    position1 = document.querySelector(".position1");
-    position2 = document.querySelector(".position2");
-    position3 = document.querySelector(".position3");
-    position4 = document.querySelector(".position4");
-    position5 = document.querySelector(".position5");
-    position6 = document.querySelector(".position6");
-    position7 = document.querySelector(".position7");
-    position8 = document.querySelector(".position8");
-    position9 = document.querySelector(".position9");
+  
 }
 
-function checkWinner() {
+// function gameFlow() {
+//     var whosTurn = player1.name;
+//     let displayWhosTurn = document.querySelector(".displayWhosTurn");
+//     displayWhosTurn.textContent = player1.name;
+    
+//     index = gameBoard.gameArray.findIndex(items => items.uniqueId == parseInt(deleteBtn.className))
+//         console.log(index)
+//         //myLibrary.splice(index, 1)
+        
+    
 
-
-    if(position1.textContent == "x" && position2.textContent == "x" && position3.textContent == "x") {console.log("x is winner")}
-    else if(position4.textContent == "x" && position5.textContent == "x" && position6.textContent == "x") {console.log("x is winner")}
-    else if(position7.textContent == "x" && position8.textContent == "x" && position9.textContent == "x") {console.log("x is winner")}
-
-    else if(position1.textContent == "x" && position4.textContent == "x" && position7.textContent == "x") {console.log("x is winner")}
-    else if(position2.textContent == "x" && position5.textContent == "x" && position8.textContent == "x") {console.log("x is winner")}
-    else if(position3.textContent == "x" && position6.textContent == "x" && position9.textContent == "x") {console.log("x is winner")}
-
-    else if(position1.textContent == "x" && position5.textContent == "x" && position9.textContent == "x") {console.log("x is winner")}
-    else if(position3.textContent == "x" && position5.textContent == "x" && position7.textContent == "x") {console.log("x is winner")}
-
-
-
-    else if(position1.textContent == "o" && position2.textContent == "o" && position3.textContent == "o") {console.log("o is winner")}
-    else if(position4.textContent == "o" && position5.textContent == "o" && position6.textContent == "o") {console.log("o is winner")}
-    else if(position7.textContent == "o" && position8.textContent == "o" && position9.textContent == "o") {console.log("o is winner")}
-
-    else if(position1.textContent == "o" && position4.textContent == "o" && position7.textContent == "o") {console.log("o is winner")}
-    else if(position2.textContent == "o" && position5.textContent == "o" && position8.textContent == "o") {console.log("o is winner")}
-    else if(position3.textContent == "o" && position6.textContent == "o" && position9.textContent == "o") {console.log("o is winner")}
-
-    else if(position1.textContent == "o" && position5.textContent == "o" && position9.textContent == "o") {console.log("o is winner")}
-    else if(position3.textContent == "o" && position5.textContent == "o" && position7.textContent == "o") {console.log("o is winner")}
-
-
-}
+// }
