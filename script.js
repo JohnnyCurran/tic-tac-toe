e31999c74a57c5d94450ecabd22ea3f6b3ce0b16
Matthew Spence <mspence5555@gmail.com>
2021-11-08 21:55:17 -0600

    finished checkWinner() logic

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let gameBoard = {
                 ["7", "8", "9"]],
 }
 
-let player1 = players("player 1", "x");
-let player2 = players("player 2", "o");
+const player1 = players("player 1", "x");
+const player2 = players("player 2", "o");
 
 
 function initGame () {
@@ -22,15 +22,15 @@ function initGame () {
     gameBoardDivs.forEach((div) => {
         counter++;
         div.classList.add(counter);
-        this["div" + counter] = document.getElementsByClassName([div.classList[1]]);
+        //this["div" + counter] = document.getElementsByClassName([div.classList[1]]);
         
         div.addEventListener("click", () => {
             //first check if div has already been played
             if(div.textContent == "x" || div.textContent == "o") {return}
+            
             //if div is unplayed loop through gameBoard.gameArray to find the index...
-            //of pointerNum (this div class number (1-9) that was clicked)...
+            //of pointerNum (witch is this div class number (1-9) that was clicked)...
             //and also splice the play ("x" or "o") into the array.
-
             if(whosTurn == player1.name) {
                 whosTurn = player2.name;
                 div.textContent = player1.symbol;
@@ -61,45 +61,64 @@ function initGame () {
         });
     });
 };
+
 function checkWinner() {
-    if(gameBoard.gameArray[0][0] == player1.symbol && 
-        gameBoard.gameArray[0][1] == player1.symbol && 
-        gameBoard.gameArray[0][2] == player1.symbol) {
-            announceWinner();
+    for(let x = 0; x < 3; x++) {
+        //player 1 vertical
+        if(gameBoard.gameArray[0][x] == player1.symbol && 
+            gameBoard.gameArray[0 + 1][x] == player1.symbol && 
+            gameBoard.gameArray[0 + 2][x] == player1.symbol) {
+                announceWinner(player1.name, player1.symbol);
         }
-    else if(gameBoard.gameArray[1][0] == player1.symbol && 
-            gameBoard.gameArray[1][1] == player1.symbol && 
-            gameBoard.gameArray[1][2] == player1.symbol) {
-            announceWinner();   
+        //player 1 horizontal
+        else if(gameBoard.gameArray[x][0] == player1.symbol && 
+            gameBoard.gameArray[x][0 + 1] == player1.symbol && 
+            gameBoard.gameArray[x][0 + 2] == player1.symbol) {
+                announceWinner(player1.name, player1.symbol);
+        }
+       //player 2 vertical
+        if(gameBoard.gameArray[0][x] == player2.symbol && 
+            gameBoard.gameArray[0 + 1][x] == player2.symbol && 
+            gameBoard.gameArray[0 + 2][x] == player2.symbol) {
+                announceWinner(player2.name, player2.symbol);
+        }
+        //player 2 horizontal
+        else if(gameBoard.gameArray[x][0] == player2.symbol && 
+            gameBoard.gameArray[x][0 + 1] == player2.symbol && 
+            gameBoard.gameArray[x][0 + 2] == player2.symbol) {
+                announceWinner(player2.name, player2.symbol);
+        }
     }
+        //player 1 diagonal
+        if(gameBoard.gameArray[0][0] == player1.symbol && 
+            gameBoard.gameArray[1][1] == player1.symbol && 
+            gameBoard.gameArray[2][2] == player1.symbol) {
+                announceWinner(player1.name, player1.symbol);
+        }
+        else if(gameBoard.gameArray[0][2] == player1.symbol && 
+            gameBoard.gameArray[1][1] == player1.symbol && 
+            gameBoard.gameArray[2][0] == player1.symbol) {
+                announceWinner(player1.name, player1.symbol);
+        }
+        //player 2 diagonal
+        else if(gameBoard.gameArray[0][0] == player2.symbol && 
+            gameBoard.gameArray[1][1] == player2.symbol && 
+            gameBoard.gameArray[2][2] == player2.symbol) {
+                announceWinner(player2.name, player2.symbol);
+        }
+        else if(gameBoard.gameArray[0][2] == player2.symbol && 
+            gameBoard.gameArray[1][1] == player2.symbol && 
+            gameBoard.gameArray[2][0] == player2.symbol) {
+                announceWinner(player2.name, player2.symbol);
+        }
+    
 }
 // [["1", "2", "3"], 
 // ["4", "5", "6"],
 // ["7", "8", "9"]],
-// if(position1.textContent == "x" && position2.textContent == "x" && position3.textContent == "x") {console.log("x is winner")}
-//     else if(position4.textContent == "x" && position5.textContent == "x" && position6.textContent == "x") {console.log("x is winner")}
-//     else if(position7.textContent == "x" && position8.textContent == "x" && position9.textContent == "x") {console.log("x is winner")}
 
-//     else if(position1.textContent == "x" && position4.textContent == "x" && position7.textContent == "x") {console.log("x is winner")}
-//     else if(position2.textContent == "x" && position5.textContent == "x" && position8.textContent == "x") {console.log("x is winner")}
-//     else if(position3.textContent == "x" && position6.textContent == "x" && position9.textContent == "x") {console.log("x is winner")}
-
-//     else if(position1.textContent == "x" && position5.textContent == "x" && position9.textContent == "x") {console.log("x is winner")}
-//     else if(position3.textContent == "x" && position5.textContent == "x" && position7.textContent == "x") {console.log("x is winner")}
-
-
-
-//     else if(position1.textContent == "o" && position2.textContent == "o" && position3.textContent == "o") {console.log("o is winner")}
-//     else if(position4.textContent == "o" && position5.textContent == "o" && position6.textContent == "o") {console.log("o is winner")}
-//     else if(position7.textContent == "o" && position8.textContent == "o" && position9.textContent == "o") {console.log("o is winner")}
-
-//     else if(position1.textContent == "o" && position4.textContent == "o" && position7.textContent == "o") {console.log("o is winner")}
-//     else if(position2.textContent == "o" && position5.textContent == "o" && position8.textContent == "o") {console.log("o is winner")}
-//     else if(position3.textContent == "o" && position6.textContent == "o" && position9.textContent == "o") {console.log("o is winner")}
-
-//     else if(position1.textContent == "o" && position5.textContent == "o" && position9.textContent == "o") {console.log("o is winner")}
-//     else if(position3.textContent == "o" && position5.textContent == "o" && position7.textContent == "o") {console.log("o is winner")}
-
-function announceWinner() {
-    alert("Winner!")
+function announceWinner(name, winner) {
+    alert(`${name}:"${winner}" is the winner! `)
 }
+
+
