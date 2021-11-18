fc1f4408c6e27c944a9f7ffd169c37bf27b0bf8d
Matthew Spence <mspence5555@gmail.com>
2021-11-18 10:20:29 -0600

    finished game win logic

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+//todo: fix bug - on a diagonal win, the losing player's symbol appears on the board and in the gameMatrix
 let player = (name, symbol, winCount) => {
     return {name, symbol, winCount}
 }
 
-const player1 = player("player 1", "X", 0);
-const player2 = player("player 2", "O", 0);
+let player1 = player("player 1", "X", 0);
+let player2 = player("player 2", "O", 0);
 
 
 
@@ -25,25 +26,15 @@ const displayModule = (() => {
         document.getElementById("player1Name").textContent = player1.name;
         document.getElementById("player1WinCount").textContent = player1.winCount;
         document.getElementById("player2Name").textContent = player2.name;
-        document.getElementById("player2WinCount").textContent = player1.winCount;
+        document.getElementById("player2WinCount").textContent = player2.winCount;
 
-        
-        
         playerMoveModule.move()
     };
 
     
-    let populateMatrix = () => {
-        // for(let x = 0; x < gameMatrix.length; x++) {
-        //     for(let y = 0; y < gameMatrix[x].length; y++) {
-        //         gameMatrix[x][y] = x.toString() + "," + y.toString()
-        //     }
-        // }
-        
-    }
-
 
     let playMove = (symbol, row, column, event) => {
+        playerMoveModule.playCounter++;
         gameMatrix[row][column] = symbol;
         event.textContent = symbol;
         winModule.checkWinner();
@@ -51,29 +42,43 @@ const displayModule = (() => {
     }
     
     let clearDisplay = () => {
-       for(let x = 0; x < gameBoardDivs.length; x++) {
+
+        playerMoveModule.playCounter = 0;
+        for(let x = 0; x < gameMatrix.length; x++) {
+            for(let y = 0; y < gameMatrix[x].length; y++) {
+                gameMatrix[x][y] = "";
+            };
+        };
+        for(let x = 0; x < gameBoardDivs.length; x++) {
            gameBoardDivs[x].textContent = "";
-       }
+        };
+       populateDisplay();
     };
+
+    let clearGame = () => {
+        player1 = player("player 1", "X", 0);
+        player2 = player("player 2", "O", 0);
+        winModule.isWinner = false;
+        playerMoveModule.playCounter = 0;
+        // possibly let loser go first in a fresh game if full game already played
+        //playerMoveModule.whosTurn = ;
+    }
     
     return {
         
         populateDisplay,
         clearDisplay,
-        populateMatrix,
         playMove,
-        gameMatrix
+        gameMatrix,
+        clearGame
     };
-    
 
 })();
-
 
 let playerMoveModule = (() => {
     
     let whosTurn = player1;
     let playCounter = 0;
-    
     
     let move = () => {
 
@@ -89,13 +94,15 @@ let playerMoveModule = (() => {
                 //player 1 turn
                 if(playerMoveModule.whosTurn.name == player1.name) {
                     
-                    displayModule.playMove(player1.symbol, event.target.dataset.row, event.target.dataset.column, event.target);
+                    displayModule.playMove(player1.symbol, event.target.dataset.row, 
+                        event.target.dataset.column, event.target);
                     playerMoveModule.whosTurn = player2;
                 }
                 // player 2 turn
                 else if(playerMoveModule.whosTurn.name == player2.name) {
                     
-                    displayModule.playMove(player2.symbol, event.target.dataset.row, event.target.dataset.column, event.target);
+                    displayModule.playMove(player2.symbol, event.target.dataset.row, 
+                        event.target.dataset.column, event.target);
                     playerMoveModule.whosTurn = player1;
                 };
             });
@@ -112,48 +119,74 @@ let playerMoveModule = (() => {
 
 let winModule = (() => {
 
-    
-    
-    
+    let isWinner = false;
+
     let checkWinner = () => {
-        
+        console.log(playerMoveModule.playCounter)
         let s = playerMoveModule.whosTurn.symbol;
-        let winArray = [[s, s, s],
-                        [s, s, s],
-                        [s, s, s]];
+        let winArray = [[s, s, s], [s, s, s], [s, s, s]];
+
         // horizontal match
         for(let x = 0, y = 0; x < displayModule.gameMatrix.length; x++) {
             if(displayModule.gameMatrix[x][y] == winArray[x][y] &&
                 displayModule.gameMatrix[x][y + 1] == winArray[x][y + 1] &&
                 displayModule.gameMatrix[x][y + 2] == winArray[x][y + 2]) {
-                console.log("horizontal match")
+                    playerMoveModule.whosTurn.winCount++;
+                    isWinner = true;
+                    console.log("horizontal match")
+                    announceWinner()
               }
             // vertical match
             else if(displayModule.gameMatrix[y][x] == winArray[y][x] &&
                 displayModule.gameMatrix[y + 1][x] == winArray[y + 1][x] &&
                 displayModule.gameMatrix[y + 2][x] == winArray[y + 2][x]) {
-                console.log("vertical match")
+                    playerMoveModule.whosTurn.winCount++;
+                    isWinner = true;
+                    console.log("vertical match")
+                    announceWinner()
               }
           };
-          // diagonal
+
+          // diagonal match
           for(let x = 0, y = 0; x < 1; x++) {
             if(displayModule.gameMatrix[x][y] == winArray[x][y] &&
                 displayModule.gameMatrix[x + 1][y + 1] == winArray[x + 1][y + 1] &&
                 displayModule.gameMatrix[x + 2][y + 2] == winArray[x + 2][y + 2]) {
-                console.log("diagonal match \\")
+                    playerMoveModule.whosTurn.winCount++;
+                    isWinner = true;
+                    console.log("diagonal match \\")
+                    announceWinner()
               }
             else if(displayModule.gameMatrix[x][y + 2] == winArray[x][y + 2] &&
                 displayModule.gameMatrix[x + 1][y + 1] == winArray[x + 1][y + 1] &&
                 displayModule.gameMatrix[x + 2][y] == winArray[x + 2][y]) {
-              console.log("diagonal match /")
+                    playerMoveModule.whosTurn.winCount++;
+                    isWinner = true;
+                    console.log("diagonal match /")
+                    announceWinner()
             }
           }
-        
+          // check for a tie
+          if(isWinner == false && playerMoveModule.playCounter > 8) {
+              console.log('tie')
+          }
+    };
+
+
+    let announceWinner = () => {
+        if(playerMoveModule.whosTurn.winCount > 2) {
+            alert(playerMoveModule.whosTurn.name + " is the champion. game over.");
+            displayModule.clearGame();
+        };
+
+        alert(playerMoveModule.whosTurn.name + " is the winner");
+        displayModule.clearDisplay();
     };
 
     return {
-        checkWinner
+        checkWinner,
+        isWinner,
+        announceWinner
     };
 
 })();
-//displayModule.populateMatrix();
