07dd285690d095bd85c0d432f6c85d43424c65eb
Johnny Curran <18315178+JohnnyCurran@users.noreply.github.com>
2021-11-20 18:50:21 -0600

    code comments

--- script.js
+++ script.js
@@ -6,12 +6,8 @@ let player = (name, symbol, winCount) => {
 let player1 = player("player 1", "X", 0);
 let player2 = player("player 2", "O", 0);
 
-
-
 const displayModule = (() => {
-
     let gameBoardDivs = document.getElementsByClassName("gameBoardDivs");
-    
     let gameMatrix = 
        [["", "", ""],
         ["", "", ""],
@@ -79,20 +75,23 @@ const displayModule = (() => {
 })();
 
 let playerMoveModule = (() => {
-    
     let whosTurn = player1;
     let playCounter = 0;
-    
     let move = () => {
-
         let gameBoardDivs = document.querySelectorAll(".gameBoardDivs");
 
+      // Every time you call move(), you add a new event listener to the game board divs
+      // They only need one - What would a better place for this event listener initialization be?
         gameBoardDivs.forEach((div) => {
             div.addEventListener("click", (event) => {
                 //check if div has already been played
                 if(event.target.textContent == player1.symbol ||
                     event.target.textContent == player2.symbol)
                      {return};
+              // You can simplify your if logic here
+              // What does playerMoveModule.whosTurn hold?
+              // (hint: try a console.log() on playerMoveModule.whosTurn)
+              // Do you need to determine what player is active before playing a move?
 
                 //player 1 turn
                 if(playerMoveModule.whosTurn.name == player1.name) {
@@ -124,15 +123,26 @@ let winModule = (() => {
 
     let isWinner = false;
 
-
     // If you update your gameArray to become a 2-dimensional array (matrix),
     // I bet you will find that you can greatly simplify this function
     // After all, there are only so many ways to determine a winner in a 3x3 tic-tac-toe board
+  // The checkWinner function is pretty large. I encourage you to try and break it down further.
+  // What all is the checkWinner function doing?
+  // - Check horizontal winner
+  // - Check vertical winner
+  // - Check diagonal winner
+  // - Announce winner
+  // - Clear display (if there is a winner)
+  // Break the check winner methods into their own - They should accept a game board as an argument and a player symbol and return true if there is a winner and false if not.
+  // I challenge you to do it also without creating a winArray variable to check against
     let checkWinner = () => {
         
         let s = playerMoveModule.whosTurn.symbol;
         let winArray = [[s, s, s], [s, s, s], [s, s, s]];
 
+      // To iterate over a 2-D array, you will find it easier to have a nested for-loop
+      // Rather than one for loop with 2 iteration variables (x, y)
+      // Let the for loop take care of the iteration for you
         // horizontal match
         for(let x = 0, y = 0; x < displayModule.gameMatrix.length; x++) {
             if(displayModule.gameMatrix[x][y] == winArray[x][y] &&
@@ -181,7 +191,6 @@ let winModule = (() => {
           return;
     };
 
-
     let announceWinner = () => {
         if(playerMoveModule.whosTurn.winCount > 2) {
             alert(playerMoveModule.whosTurn.name + " is the champion. game over.");
@@ -202,5 +211,4 @@ let winModule = (() => {
         isWinner,
         announceWinner
     };
-
 })();
